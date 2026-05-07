7cd338d4b98c9c91fd7d8ee8765c885a7a7e756c
Jar0705 <156736264+Jar0705@users.noreply.github.com>
2026-05-07 11:49:59 +0700

    Refactor code for fetching user data by ID

--- edit.php
+++ edit.php
@@ -5,14 +5,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Ambil ID
 $id = $_GET['id'];
 
-// Ambil data user berdasarkan ID
 $data = mysqli_query($koneksi, "SELECT * FROM user WHERE id=$id");
 $d = mysqli_fetch_array($data);
 
-// Proses update
 if(isset($_POST['update'])){
 
     $nama = $_POST['nama'];
